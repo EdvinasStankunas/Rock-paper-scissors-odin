460013365ce970fbe3b88571fb54e65958d7c41c
EdvinasStankunas <98836185+EdvinasStankunas@users.noreply.github.com>
2022-06-19 17:56:12 +0300

    Add game compare logic

--- script.js
+++ script.js
@@ -16,10 +16,40 @@ function getRandomNumber() {
   return Math.floor(Math.random() * 3 + 0);
 }
 
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
 function playRound(playerSelection, computerSelection) {
   const player = playerSelection.toLowerCase();
   const computer = computerSelection.toLowerCase();
+
+  if (player === computer) {
+    return "Tie game.";
+  }
+
+  if (player === ROCK) {
+    if (computer === PAPER) {
+      return "Computer wins! Paper beats rock.";
+    }
+
+    return "Player wins! Rock beats scissors.";
+  }
+
+  if (player === PAPER) {
+    if (computer === ROCK) {
+      return "Player wins! Paper beats rock.";
+    }
+    return "Computer wins! Scissors beat paper.";
+  }
+
+  if (player === SCISSORS) {
+    if (computer === ROCK) {
+      return "Computer wins! Rock beats scissors.";
+    }
+    return "Player wins! Scissors beat paper.";
+  }
 }
-const playerSelection = "rock";
+const playerSelection = ROCK;
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
