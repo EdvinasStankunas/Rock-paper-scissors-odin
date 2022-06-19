762277960d61ebeb8bb67fdf9f05b9f9a86cee1f
EdvinasStankunas <98836185+EdvinasStankunas@users.noreply.github.com>
2022-06-19 17:42:10 +0300

    Add random number logic

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("kanors");
+function computerPlay() {
+  const randomNumber = getRandomNumber();
+
+  if (randomNumber === 0) {
+    return "Rock";
+  }
+
+  if (randomNumber === 1) {
+    return "Paper";
+  }
+
+  return "Scissors";
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 3 + 0);
+}
+
+function playRound(playerSelection, computerSelection) {
+  const player = playerSelection.toLowerCase();
+  const computer = computerSelection.toLowerCase();
+}
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
